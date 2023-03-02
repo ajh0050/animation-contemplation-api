e5a161c9223125a822fae79121c2cd98197620b8
Adam Hughes <adamjordanhughes@gmail.com>
2023-03-02 13:40:33 -0500

    Add cors

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
 app.set('port', process.env.PORT || 3000);
 const characters = require('./data/characters');
 
